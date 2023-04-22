a9c5fe2aba804803e5d45b612747f01efa9348a4
Khooshrin Aspi Pithawalla <88196065+Khooshrin@users.noreply.github.com>
2023-04-22 09:30:09 +0530

    Visualizer fixed

--- visualization.py
+++ visualization.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Load the data from the text file
+# Load the data from the input text file
 with open('input.txt', 'r') as f:
     lines = f.readlines()[1:-1]
 
@@ -19,18 +19,30 @@ plt.ylabel('Y')
 plt.title('Segmented Least Squares')
 plt.plot(x, y, 'ro')
 
-
-# Load the data from the text file
+# Load the data from the output text file
 data = np.loadtxt('output.txt')
-slope = data[:, 0]
-intercept = data[:, 1]
-start = data[:, 2]
-end = data[:, 3]
+
+if len(data.shape) == 1:  # Only one line in the output file
+    slope = [data[0]]
+    intercept = [data[1]]
+    start = [data[2]]
+    end = [data[3]]
+else:
+    slope = data[:, 0]
+    intercept = data[:, 1]
+    start = data[:, 2]
+    end = data[:, 3]
 
 # Plot the lines
-for m, b, s, e in zip(slope, intercept, start, end):
-    x = np.linspace(s, e, 100)
-    y = m * x + b
+if len(slope) == 1:  # Only one line in the output file
+    x = np.linspace(start[0], end[0], 100)
+    y = slope[0] * x + intercept[0]
     plt.plot(x, y)
-    # Plot the data
+else:
+    for m, b, s, e in zip(slope, intercept, start, end):
+        x = np.linspace(s, e, 100)
+        y = m * x + b
+        plt.plot(x, y)
+
+# Show the plot
 plt.show()
